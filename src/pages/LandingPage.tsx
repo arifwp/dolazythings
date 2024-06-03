a966f18407203451e76ac2015d2dfcfe92e20448
arifwp <arifwahyu696@gmail.com>
2024-06-03 16:36:23 +0700

    add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import Home from "../sections/Home";
+import { Options } from "../sections/Options";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Home />
+
+      <Options />
+    </>
+  );
+};
+
+export default LandingPage;
