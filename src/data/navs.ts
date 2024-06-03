c32be286840c60ee0a19bc9e56efdc92fd90ff6b
arifwp <arifwahyu696@gmail.com>
2024-06-03 16:36:51 +0700

    add data navbar

---
+++ src/data/navs.ts
@@ -0,0 +1,19 @@
+interface NavItem {
+    link: string;
+    label: string;
+}
+
+export const navs: Array<NavItem> = [
+    {
+        link: '#homeSection',
+        label: 'Home'
+    },
+    {
+        link: '#aboutSection',
+        label: 'About'
+    },
+    {
+        link: '#filterSection',
+        label: 'Filter'
+    },
+];
